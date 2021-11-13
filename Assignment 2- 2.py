c02d898d046667f9bc27830184de9b422b908b98
dimpledagar123 <94053231+dimpledagar123@users.noreply.github.com>
2021-11-13 23:34:37 +0530

    Add files via upload

---
+++ Assignment 2- 2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Write a Python program to print a dictionary whose keys should be the alphabet from a-z and the value should be corresponding ASCII values
+# 
+# 
+# 
+# Sample Output : {'a': 97, 'b': 98, 'c': 99, 'd': 100, 'e': 101, 'f': 102, 'g': 103, 'h': 104, 'i': 105, 'j': 106, 'k': 107, 'l': 108, 'm': 109, 'n': 110, 'o': 111, 'p': 112, 'q': 113, 'r': 114, 's': 115, 't': 116, 'u': 117, 'v': 118, 'w': 119, 'x': 120, 'y': 121, 'z': 122}
+
+# In[3]:
+
+
+small_alp=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+d={}
+for i in small_alp:
+    d.update({i:ord(i)})
+print(d)
+
