56956eac9ebba06a9670a7b8cfcff8c43a79740a
dimpledagar123 <94053231+dimpledagar123@users.noreply.github.com>
2021-12-04 15:33:22 +0530

    Add files via upload

---
+++ Assignment - 4 - 1 .py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Write a Python program to create a lambda function that adds 25 to a given number passed in as an argument.
+# 
+# 
+# 
+# sample input: 10
+# 
+# sample output: 35
+
+# In[2]:
+
+
+fun = lambda d,s : d+s
+fun(10,25)
+
